18f32d7daecdcb6750f0bc7d5f4b495cac8f7372
elijah obara <elijahobara357@gmail.com>
2023-05-08 23:14:23 +0300

    auction items

--- apps/aunctionItem/views.py
+++ apps/aunctionItem/views.py
@@ -12,5 +12,28 @@ import json
 from django.core.mail import send_mail
 
 
-
+def all_item(request,category_slug=None):
+    products=Lot.objects.filter(is_trending=True)
+    category=None
+    categories=Category.objects.all()
+    
+    print(category_slug)
+    if category_slug:
+        category=get_object_or_404(Category,slug=category_slug)
+        products = products.filter(category=category)
+    print(category)
+    
+    items_trending = Lot.objects.filter(is_trending=True)
+    paginator =Paginator(items_trending, 6) 
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number) 
+    context={
+        'items':page_obj,
+        'products':products,
+        'categories':categories,
+        'category':category, 
+        
+    }
+    return render(request,'all_item.html',context)
+    
 
