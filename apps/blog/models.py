1de92fa86c51ff1e5d6e6533f925944b1481ebb3
elijah obara <elijahobara357@gmail.com>
2023-05-08 11:43:49 +0300

    blog app model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -29,7 +29,7 @@ class Post(models.Model):
     tags=TaggableManager() #it allows to add,retrive and remove tags from post.
     
     class Meta:
-        ordering=('-publish',) 
+        ordering=('-published',) 
         
     def get_absolute_url(self):
         return reverse('blog-app:post-detail',
@@ -38,3 +38,19 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    
+class Comment(models.Model):
+    post=models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    body=models.TextField()
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now_add=True)
+    active=models.BooleanField(default=True)
+    
+    class Meta:
+        ordering=('created',)
+        
+    def __str__(self):
+        return f" Comments by {self.name} on {self.post}"
