2ceaea294b5edbcf0c964e25a4a39680de164015
elijah obara <elijahobara357@gmail.com>
2023-05-08 11:14:16 +0300

    models design

--- apps/aunctionItem/models.py
+++ apps/aunctionItem/models.py
@@ -32,3 +32,32 @@ class Seller(models.Model):
     def __str__(self):
         return self.name
     
+class Lot(models.Model):
+    category=models.ForeignKey(Category, related_name='lots', on_delete=models.CASCADE)
+    seller =models.ForeignKey(Seller,on_delete=models.DO_NOTHING)
+    slug=models.SlugField(max_length=200,db_index=True)
+    product_name=models.CharField(max_length=200,db_index=True)
+    is_live=models.BooleanField(default=False)
+    base_price=models.IntegerField(default=0.0)
+    current_price=models.IntegerField(default=0.0)
+    description=models.CharField(max_length=500)
+    main_photo=models.ImageField(upload_to='photos/main/%Y/%m/%d/')
+    photo1=models.ImageField(upload_to='photos/optional/%Y/%m/%d/',blank=True)
+    photo2=models.ImageField(upload_to='photos/optional/%Y/%m/%d/',blank=True)
+    photo3=models.ImageField(upload_to='photos/optional/%Y/%m/%d/',blank=True)
+    photo4=models.ImageField(upload_to='photos/optional/%Y/%m/%d/',blank=True)
+    is_trending=models.BooleanField(default=False)
+    on_banner=models.BooleanField(default=False)
+    year_published=models.DateTimeField(default=datetime.now()) 
+    
+    def get_absolute_url(self):
+        return reverse('auction:single-item',args=[self.id,self.slug])
+    
+    class Meta:
+        ordering=('product_name',)
+        index_together=(('id','slug'),)
+    
+    
+    def __str__(self):
+        return self.product_name
+    
